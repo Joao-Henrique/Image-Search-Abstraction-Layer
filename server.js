9b44bf3e507cde3a38d69bec144b56dbe8fc87bb
Joao Henrique <jppulicidade@gamail.com>
2017-09-07 00:09:52 +0100

    Allmost done. Missing minor ajustments

--- server.js
+++ server.js
@@ -27,38 +27,91 @@ db.once('open', () => {
 
 
 ////////////////////////////////////////////////////
-////   GET ALL SEARCH TERMS FROM THE DATABASE   ////
-app.get('/api/recentsearchs', (req, res, next) => {
-  searchTerm.find({}, (err, data) => 
+////  GET ALL RECENTSEARCHES FROM THE DATABASE  ////
+app.get('/api/recentsearchs', (req, res) => {
+  searchTerm.find({}).sort({date:-1}).exec((err, data) => 
     {
       res.json(data);
-    })
-})
+    }
+)})
 // END /////////////////////////////////////////////
 ////////////////////////////////////////////////////
 
 
 ////////////////////////////////////////////////////
 ////  GET CALL WITH PARAMS TO SEARCH FOR IMAGES  ///
-app.get('/api/imagesearch/:searchVal*', (req, res, next) => {
+app.get('/api/imagesearch/:searchVal*', (req, res) => {
   var { searchVal } = req.params;
   var { offset } = req.query;
-
   var data = new searchTerm({
     searchVal,
     searchDate: new Date()
   });
-
+// SAVE DATA TO COLLECTION
   data.save(err => {
     if (err) {
       res.send('Error Saving to database');
     }
-    res.json(data);
+// PREVENT APP CRASH IF NO OFFSET IS DEFINED
+    var searchOffset;
+    if (searchOffset === undefined) {
+      searchOffset=1;
+      offset=0;
+    }
+// DEFINING OFFSET
+    if (offset){
+      if (offset == 1) {
+        offset = 0;
+        searchOffset = 1;
+      }
+      else if (offset > 1) {
+        searchOffset = offset + 1;
+      }
+      else {
+        offset = 0;
+        searchOffset = 1;
+      }
+    }
+// WORK WITH BING API
+    Bing.images(searchVal, {
+      top: ( 10 * searchOffset ),
+      skip: ( 10 * offset )
+    }, function(error, rez, body){
+      var bingData = [];
+
+      for (var i=0; i<10; i++){
+        bingData.push({
+          url: body.value[i].webSearchUrl,
+          snippet: body.value[i]. name,
+          tumbnail: body.value[i].thumbnailUrl,
+          context: body.value[i].hostPageDisplayUrl
+        });
+      }
+      res.json(bingData);
+    })
   });
- // return res.json({
- //   searchVal,
- //   offset
- // });
+}); 
+// END /////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////
+////  GET CALL WITH PARAMS TO SEARCH FOR IMAGES  ///
+app.get('/api/imagesearch/latest/imagesearch', (req, res) => {
+  var { searchVal } = req.params;
+  var { offset } = req.query;
+  var data = new searchTerm({
+    searchVal,
+    searchDate: new Date()
+  });
+// SAVE DATA TO COLLECTION
+  data.save(err => {
+    if (err) {
+      res.send('Error Saving to database');
+    }
+
+    res.json(bingData)
+  });
 }); 
 // END /////////////////////////////////////////////
 ////////////////////////////////////////////////////
